c2af9e9692b0c439e3847b93ee018840db017fec
j-stapera <haltheretic@gmail.com>
2024-04-12 21:08:35 -0500

    removed commented code

--- src/Actions.java
+++ src/Actions.java
@@ -1,18 +1,7 @@
 import java.util.ArrayList;
 
 public class Actions {
-//    private static final VictimPicker victimPicker = new VictimPicker();
-//    private static final VictimLabel victimLabel = new VictimLabel();
-//    private static final TimerLabel timerLabel = new TimerLabel();
-//    private static final TimerLogic timerLogic = new TimerLogic(timerLabel);
 
-
-    /**
-     * Creates a set of 4 clickable buttons (add point, remove point, mark absent, timer panel)
-     * see TimerButtonPanel for info on timer panel
-     */
-
-    
     public static void addPoint(VictimPicker vp, VictimLabel vl) {
         vp.getCurrentVictim().addScore();
         vl.updateText(vp.getCurrentVictim());
