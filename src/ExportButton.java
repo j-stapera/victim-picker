c34563bfeb1acf7d7acc46c602ed410a5b62f0c4
j-stapera <raventhegray@yahoo.com>
2024-04-23 01:26:15 -0500

    Change constructor to require VictimPicker, made call Action.export

--- src/ExportButton.java
+++ src/ExportButton.java
@@ -3,8 +3,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ExportButton extends JButton implements ActionListener {
-
-    ExportButton(){
+    VictimPicker victimPicker;
+    ExportButton(VictimPicker vp){
+        victimPicker = vp;
         this.setBounds(1035,0,200,50);
         this.setFocusable(false);
         this.addActionListener(this);
@@ -14,6 +15,7 @@ public class ExportButton extends JButton implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==this){
+            Actions.exportExportables(victimPicker);
             //action to export all necessary info to file(s)
         }
     }
