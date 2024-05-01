52d3fdac0b440b95aad4db90ea894d5e9976c005
Heather <hwatson7@cub.uca.edu>
2024-05-01 18:45:22 -0500

    Minor changes to QuestionPicker. Not fully functional yet needs code to pass in questions properly in text file format

--- src/QuestionPicker.java
+++ src/QuestionPicker.java
@@ -1,11 +1,33 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class QuestionPicker {
 
     private ArrayList<String> Questions;
 
+    String Question;
+
     private ArrayList<String> AlreadyAnswered;
 
+    public QuestionPicker(){
+        Questions = new ArrayList<>();
+
+    }
+
+
+    public String chooseQuestion(){
+
+        Random rand = new Random();
+
+        int index = rand.nextInt(Questions.size());
+
+        Question = Questions.get(index);
+
+        Questions.remove(index);
+
+        return Question;
+    }
 
 
 
