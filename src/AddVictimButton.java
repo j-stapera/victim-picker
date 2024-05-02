bf13f9bd3d4e722ae7d353b4f99f7450272e6ebb
Abraham Martinez <amartinezbrito@cub.uca.edu>
2024-05-02 16:11:48 -0500

    12

--- src/AddVictimButton.java
+++ src/AddVictimButton.java
@@ -4,18 +4,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+
+//this class represents a custom JButton that when clicked adds a victim
 public class AddVictimButton extends JButton implements ActionListener {
 
     //private ArrayList<VictimPanel> victimPanels;
+    //declare an instance variable to hold the number of victims
     private VictimNumber numOfVictims;
 
+    //constructor for the AddVictimButton class
     AddVictimButton (VictimNumber numOfVictims) {
+        //initialize the numofvictims instance variable with the passed Victimnumber object
         this.numOfVictims = numOfVictims;
         //this.victimPanels = victimPanels;
-
+        //set the position and size of the button on the JFrame
         this.setBounds(335,525,150,50);
+        //ensure the button doesnt receive focus
         this.setFocusable(false);
+        //handles button clicks
         this.addActionListener(this);
+        //set the display on the button
         this.setText("Add Victim");
 
         //One victim by default on screen
@@ -23,9 +31,10 @@ public class AddVictimButton extends JButton implements ActionListener {
 
     }
 
-
+    //action listener implementation for handling button clicks
     @Override
     public void actionPerformed(ActionEvent e) {
+        //when button is clicked \, call the addvictim method of the numofvictims object
         numOfVictims.addVictim();
     }
 }
--- src/PickVictimButton.java
+++ src/PickVictimButton.java
@@ -4,15 +4,24 @@ import java.awt.event.ActionListener;
 
 public class PickVictimButton extends JButton implements ActionListener {
 
+    //instance variable holds refence to the victim panel
     private final VictimPanel victimPanel;
+    //constructor
     PickVictimButton(VictimPanel victimPanel){
+        //initialize the victim instance variable with the passed victimpanel object
         this.victimPanel = victimPanel;
+        //set the text displayed on the button
         this.setText("Pick Victim");
+        //add the actionlistener to handle button clicks
         this.addActionListener(this);
         this.setFocusable(false);
     }
+    // implementation for handling button clicks
     @Override
     public void actionPerformed(ActionEvent e) {
+        //when the button is clicked, call the selectVictim method from the Actions class
+        //passing the VictimPicker from the VictimPanel and the VictimPanel itself
         Actions.selectVictim(victimPanel.getVictimPicker(), victimPanel);
+
     }
 }
