3a39b999bb95da283e27b266c34216cc0882bc90
j-stapera <raventhegray@yahoo.com>
2024-05-02 12:26:28 -0500

    actually changes markAbsent bool now

--- src/MarkAbsentButton.java
+++ src/MarkAbsentButton.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 public class MarkAbsentButton extends JButton implements ActionListener {
     VictimPanel victimPanel;
     JButton thisButton;
+    boolean markedAbsent;
     MarkAbsentButton(VictimPanel victimPanel){
         thisButton = this;
         this.victimPanel = victimPanel;
@@ -13,22 +14,29 @@ public class MarkAbsentButton extends JButton implements ActionListener {
         this.addActionListener(this);
         this.setFocusable(false);
         //this.setFont(new Font("Times New Roman", Font.PLAIN,25));
+        markedAbsent = false;
 
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (victimPanel.getVictim().getName().equals("Pick Victim")) {
-            Actions.markAbsent(victimPanel.getVictim(), victimPanel);
-
-            this.setText("Marked!");
-            Timer timer = new Timer(2000, new ActionListener() {
-                public void actionPerformed(ActionEvent evt) {
-                    // Change the button text back to the original
-                    thisButton.setText("Mark Absent");
-                }
-            });
-            timer.setRepeats(false); // Only execute once
-            timer.start();
+        if (!victimPanel.getVictim().getName().equals("Pick Victim")) {
+            if (!markedAbsent) {
+                markedAbsent = true;
+                Actions.markAbsent(victimPanel.getVictim(), victimPanel);
+                this.setText("Marked!");
+            } else {
+                markedAbsent = false;
+                Actions.unmarkAbsent(victimPanel.getVictim(),victimPanel);
+                this.setText("Mark Absent");
+            }
+//            Timer timer = new Timer(2000, new ActionListener() {
+//                public void actionPerformed(ActionEvent evt) {
+//                    // Change the button text back to the original
+//                    thisButton.setText("Mark Absent");
+//                }
+//            });
+//            timer.setRepeats(false); // Only execute once
+//            timer.start();
         }
     }
 }
