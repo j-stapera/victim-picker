d6de61fad74b4dd1e440779741da830cea92454e
j-stapera <raventhegray@yahoo.com>
2024-05-09 22:48:02 -0500

    fixed start timer button not updating

--- src/Actions.java
+++ src/Actions.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Actions {
@@ -31,13 +32,13 @@ public class Actions {
     }
 
 
-
-
-    public static void toggleTimer(TimerLogic tl) {
+    public static void toggleTimer(TimerLogic tl, JButton startButton) {
         if (tl.isRunning()) {
             tl.stopTimer();
+            startButton.setText("<html>Start<br/>Timer<html/>");
         } else {
             tl.startTimer();
+            startButton.setText("<html>Stop<br/>Timer<html/>");
         }
     }
 
